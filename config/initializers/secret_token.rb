ce74d44664dc63446d5547f776992475dd769634
Aurélien Gasser <aurelien.gasser@transcovo.com>
2013-06-18 22:44:01 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sauba::Application.config.secret_token = '2b2482c7a570658f674344d26bd39fe8c69668401746d49d4d184ef97aae7509405854c3060dd1beb95ef514689d27a615f6384157b816fc349f228a7e142864'
